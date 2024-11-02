73a346e8e93aa8d8033f73a8f8648c505034e8b6
Yang Seuljin <gluestuck@naver.com>
2024-11-02 22:28:48 +0900

    ENH: 생성자 추가

--- src/main/java/com/sideproject/boardserver/dto/CommentDTO.java
+++ src/main/java/com/sideproject/boardserver/dto/CommentDTO.java
@@ -1,14 +1,13 @@
 package com.sideproject.boardserver.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Builder
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDTO {
     private int id;
     private int postId;
